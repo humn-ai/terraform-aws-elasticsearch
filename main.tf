fe15cc7ce9b07e9d821bcbe440011796577e7658
Callum <callumccr@gmail.com>
2021-04-06 11:44:53 +0100

    updated modules domain policy

--- main.tf
+++ main.tf
@@ -210,7 +210,7 @@ resource "aws_elasticsearch_domain" "default" {
 }
 
 data "aws_iam_policy_document" "default" {
-  count = module.this.enabled && (length(var.iam_authorizing_role_arns) > 0 || length(var.iam_role_arns) > 0) ? 1 : 0
+  count = module.this.enabled && (length(var.iam_authorizing_role_arns) > 0 || length(var.allowed_iam_role_arns) > 0) ? 1 : 0
 
   statement {
     effect = "Allow"
